fb4c05b4f400ea2b3f29f93538a2148eca7a1553
김영현 <rethe08@naver.com>
2022-04-14 14:10:30 +0900

    Add files via upload
    초기 파일 업로드 테스트입니다.

---
+++ src/main/java/EmpManager/EmpManager.java
@@ -0,0 +1,8 @@
+package EmpManager;
+
+public class EmpManager {
+
+    public void start(String[] args){
+        System.out.println("HelloWorld");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import EmpManager.EmpManager;
+
+public class Main {
+
+    public static void Main(String[] args){
+
+        new EmpManager().start(args);
+    }
+}
